07c18e2d256b3b4d510ce340509d0c7f9a8afdcb
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2026-05-20 10:45:08 +0200

    initial state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideSignalFormsConfig } from '@angular/forms/signals';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideSignalFormsConfig({
+      classes: {
+        'invalid': ({state}) => state().invalid()
+      },
+    })
   ],
 };
--- src/app/playgrounds/basic-forms/basic-forms.ts
+++ src/app/playgrounds/basic-forms/basic-forms.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { email, form, FormField, required } from '@angular/forms/signals';
 
 import { Order } from '../../core/order/model';
 
@@ -13,11 +13,23 @@ export default class BasicForms {
   readonly #orderModel = signal<Order>({
     fullName: '',
     email: '',
+    // TASK 2: add the new itemCount property to the model
+    // and set initial value to it
   });
   protected readonly form = form(
     this.#orderModel,
     (path) => {
-      // validators & control behavior
+      required(path.fullName, { message: `This field is required` });
+      // *TASK 6: Apply minLength(3) validator to fullName field
+      // resolve error mesage dynamically shows how many characters allowed for the field
+      // e.g instead `The length is too short` -> `The minimum length is 3 characters` 
+      
+      email(path.email, { message: `The provided email isn't valid` });
+
+       // TASK 4: Apply validators to itemCount
+       // make it required
+       // allow to enter value between 1 and 30
+       // use required, min and max validators
     },
     {
       // form config
